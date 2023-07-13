dc803852b822ebd826bb1b3a97dde09b2708f5a8
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-07-13 20:17:39 +0100

    clang format

--- traffic_light.ino
+++ traffic_light.ino
@@ -5,31 +5,31 @@ int green_led_pin = 11;
 int led_delay_ms = 1000;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(red_led_pin, OUTPUT);
-  pinMode(orange_led_pin, OUTPUT);
-  pinMode(green_led_pin, OUTPUT);
+    // put your setup code here, to run once:
+    pinMode(red_led_pin, OUTPUT);
+    pinMode(orange_led_pin, OUTPUT);
+    pinMode(green_led_pin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(red_led_pin, HIGH);
-  digitalWrite(orange_led_pin, LOW);
-  digitalWrite(green_led_pin, LOW);
-  delay(led_delay_ms);
+    // put your main code here, to run repeatedly:
+    digitalWrite(red_led_pin, HIGH);
+    digitalWrite(orange_led_pin, LOW);
+    digitalWrite(green_led_pin, LOW);
+    delay(led_delay_ms);
 
-  digitalWrite(red_led_pin, HIGH);
-  digitalWrite(orange_led_pin, HIGH);
-  digitalWrite(green_led_pin, LOW);
-  delay(led_delay_ms);
+    digitalWrite(red_led_pin, HIGH);
+    digitalWrite(orange_led_pin, HIGH);
+    digitalWrite(green_led_pin, LOW);
+    delay(led_delay_ms);
 
-  digitalWrite(red_led_pin, LOW);
-  digitalWrite(orange_led_pin, LOW);
-  digitalWrite(green_led_pin, HIGH);
-  delay(led_delay_ms);
+    digitalWrite(red_led_pin, LOW);
+    digitalWrite(orange_led_pin, LOW);
+    digitalWrite(green_led_pin, HIGH);
+    delay(led_delay_ms);
 
-  digitalWrite(red_led_pin, LOW);
-  digitalWrite(orange_led_pin, HIGH);
-  digitalWrite(green_led_pin, LOW);
-  delay(led_delay_ms);
+    digitalWrite(red_led_pin, LOW);
+    digitalWrite(orange_led_pin, HIGH);
+    digitalWrite(green_led_pin, LOW);
+    delay(led_delay_ms);
 }
